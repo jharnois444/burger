ad4bf36efa7882c18acfecedcbd40ecc9e7d343a
jharnois444 <jharnois444@gmail.com>
2018-05-19 00:19:11 -0400

    updated server.js

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,8 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
+CREATE TABLE burgers (
+id INTEGER(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+burger_name VARCHAR(255) NOT NULL,
+devoured BOOLEAN 
+);
